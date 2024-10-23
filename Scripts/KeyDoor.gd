92313c6f5d2ddfa9cf0513b7bb58fd1b89e95ad0
LunarTides <31688109+LunarTides@users.noreply.github.com>
2024-10-23 08:26:33 +0200

    Add keys and keydoors

---
+++ Scripts/KeyDoor.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+var score = 0
+var max_score = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for key: Key in get_tree().get_nodes_in_group(&"Key"):
+		max_score += 1
+		
+		key.collected.connect(func():
+			score += 1
+			
+			if score >= max_score:
+				queue_free()
+		)
