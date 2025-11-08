5e048499aa4b546ee7f1d99a0df8db2bf01c2614
LunarTides <31688109+LunarTides@users.noreply.github.com>
2025-11-08 15:02:17 +0100

    Add cayote jump and level 8

--- Scripts/Flags/Norway.gd
+++ Scripts/Flags/Norway.gd
@@ -5,16 +5,28 @@ extends CharacterBody2D
 @export var speed := 300.0
 @export var jump_velocity := -400.0
 var is_invincible := false
+var can_cayote_jump := false
+var was_on_floor_last_frame := false
 
 
 func _physics_process(delta: float) -> void:
+	var on_floor = is_on_floor()
+	
 	# Add the gravity.
-	if not is_on_floor():
+	if not on_floor:
 		velocity += get_gravity() * delta
+		
+		if was_on_floor_last_frame:
+			can_cayote_jump = true
+		
+		was_on_floor_last_frame = false
+	else:
+		was_on_floor_last_frame = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed(&"jump") and is_on_floor():
+	if Input.is_action_pressed(&"jump") and (on_floor or can_cayote_jump):
 		velocity.y = jump_velocity
+		can_cayote_jump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis(&"move_left", &"move_right")
